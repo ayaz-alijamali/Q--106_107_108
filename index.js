ee54ac4591a1f8bf642b56ad64014303561d39a4
ayaz-alijamali <ayazalijamali64@gmail.com>
2024-05-05 09:46:44 +0500

    100 days coding challenge Questions

---
+++ index.js
@@ -0,0 +1,43 @@
+// Question __________________ 106
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// Example usage:
+const year = 2024;
+if (isLeapYear(year)) {
+    console.log(`${year} is a leap year.`);
+}
+else {
+    console.log(`${year} is not a leap year.`);
+}
+// Question ______________ 107
+function isDivisibleBy2And3(number) {
+    return number % 2 === 0 && number % 3 === 0;
+}
+// Example usage:
+const num = 12;
+if (isDivisibleBy2And3(num)) {
+    console.log(`${num} is divisible by both 2 and 3.`);
+}
+else {
+    console.log(`${num} is not divisible by both 2 and 3.`);
+}
+// Question _________________ 108
+function areStringsIdenticalIgnoreCase(str1, str2) {
+    return str1.toLowerCase() === str2.toLowerCase();
+}
+// Example usage:
+const string1 = "Hello";
+const string2 = "hello";
+if (areStringsIdenticalIgnoreCase(string1, string2)) {
+    console.log("The strings are identical, ignoring case sensitivity.");
+}
+else {
+    console.log("The strings are not identical, considering case sensitivity.");
+}
+export {};
